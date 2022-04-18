d0f78cb9e46296033f100f63854e3127bf5ccff7
Jessica Nelson <yahwehschica@yahoo.com>
2022-04-18 11:17:25 -0400

    commit tree c17b11cb7dee8e7ab150e4da8a032f6b7c3a1ab4

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+  attr_accessor :sides
+
+  @sides = []
+
+  def initialize(side_one, side_two, side_three)
+    @sides = [side_one, side_two, side_three]
+    @sides.sort!
+  end
+
+  def kind
+    if @sides.any?{|s| s <= 0} || ((@sides[0] + @sides[1] <= @sides[2]))
+      raise TriangleError
+    elsif @sides.uniq.length == 1
+      :equilateral
+    elsif @sides.uniq.length == 2
+      :isosceles
+    else 
+      :scalene
+    end
+  end
+    class TriangleError < StandardError
+    end 
+  
 end
